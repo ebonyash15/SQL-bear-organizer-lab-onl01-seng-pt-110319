df2e3783d937f6f7bb4bead2fbf32728f133fa91
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-24 23:45:48 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 65, 'female', 'blue', 'mellow', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 12, 'male', 'green', 'routy', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 50, 'female', 'red', 'catty', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 120, 'male', 'purple', 'solemn', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'female', 'chartrusse', 'friendly', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 14, 'male', 'mauve', 'cantankerous', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'male', 'pink', 'curious', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 79, 'non-BEAR-inary', 'white', 'blood-thirsty', true);
