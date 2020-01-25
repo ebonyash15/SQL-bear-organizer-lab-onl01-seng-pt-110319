c9042eff4126dc39d50f46a74cce380ec6b307e9
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-25 00:10:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 65, 'female', 'blue', 'mellow', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 12, 'male', 'green', 'routy', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 50, 'female', 'red', 'catty', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 120, 'male', 'purple', 'solemn', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'female', 'chartrusse', 'friendly', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 14, 'male', 'mauve', 'cantankerous', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'male', 'pink', 'curious', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 65, 'F', 'blue', 'mellow', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 12, 'M', 'green', 'routy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 50, 'F', 'red', 'catty', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 120, 'M', 'purple', 'solemn', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'chartrusse', 'friendly', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 14, 'M', 'mauve', 'cantankerous', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'M', 'pink', 'curious', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 79, 'non-BEAR-inary', 'white', 'blood-thirsty', 0);
